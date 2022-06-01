b5db1ff76a9b4b2a1c7bf69166037ae45877f1f5
Haley Moses <vhr825@gmail.com>
2022-06-01 15:43:24 -0500

    commiting candy dreams to github as first commit includes basic skeleton structure and started on homepage

---
+++ src/components/home/Home.js
@@ -0,0 +1,20 @@
+import './Home.scss';
+import React from 'react';
+
+const Home = () => {
+
+
+    return (
+        <div className='outterContainer'>
+        
+        
+                <div className='homeTitle'>
+                    Candy Dreams
+                </div>
+        
+        
+        </div>    
+    )
+};
+
+export default Home;
